dc104a1ab4b6bb6c9e0ad64f906d2c8b276c9d11
Anqied <angiechen@live.com>
2020-09-19 17:21:06 -0400

    Add files via upload

---
+++ my_app/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#post-button").click(function(){
+        const url = "http://localhost:5000/post";
+        const postInfo = {
+        	title: $('#title').val(),
+        	description: $('#description').val(),
+        };
+
+        $s.ajax({
+        	url: url,
+        	type: "POST",
+        	data: JSON.stringify(postInfo),
+        	processData: false,
+        	contentType: "application/json; charset=UTF-8",
+        	complete: function() {
+        		console.log("request completed!");
+        	}
+        });
+    });
+});
