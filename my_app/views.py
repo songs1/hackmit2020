dbe5df6821d9b0793f1c465e3a154ac8dcbe6c53
songs1 <71558405+songs1@users.noreply.github.com>
2020-09-19 17:16:03 -0400

    add views.py

---
+++ my_app/views.py
@@ -0,0 +1,23 @@
+from my_app import app, db
+from flask import render_template, request, redirect
+from my_app.models import User
+
+
+@app.route("/")
+def index():
+    db_users = User.query.all()
+    users_list = [{"First Initial":u.first, "Last Name":u.last, "Date of Birth":u.dob, \
+    "County":u.county, "Phone Number":u.phone, "Email":u.email} for u in db_users]
+
+    return render_template("index.html", users = users_list)
+    
+
+@app.route("/add_user", methods=['POST'])
+def add_user():
+	if request.method == "POST":
+		info = request.get_json()
+		new_user = User(first=info['first'], last=info['last'], dob=info['dob'], \
+		county=info['county'], phone=info['phone'], email=info['email']
+		db.session.add(new_user)
+		db.session.commit()
+	return redirect("/")
