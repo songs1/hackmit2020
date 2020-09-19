71da0230cb1f8645850e4c636cec48f4d4607629
chentoria <71535196+chentoria@users.noreply.github.com>
2020-09-19 19:03:32 -0400

    update: remove phone and add county

--- my_app/models.py
+++ my_app/models.py
@@ -7,5 +7,5 @@ class User(db.Model):
 	first = db.Column(db.String(1))
 	last = db.Column(db.String(50))
 	dob = db.Column(db.Integer)
-	phone = db.Column(db.Integer)
+	county = db.Column(db.String(50))
 	email = db.Column(db.String(50))
