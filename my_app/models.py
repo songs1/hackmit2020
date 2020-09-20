f84b948629727268c14a3c2f352e8740bdd3be47
Jason Gao <jgaominneapolis@gmail.com>
2020-09-20 09:50:52 -0400

    adds models

--- my_app/models.py
+++ my_app/models.py
@@ -1,11 +1,12 @@
-#first, last, dob, county, phone, email
+#first, last, dob, county, phone, email, status
 
 from my_app import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, primary_key=True)
     first = db.Column(db.String(1))
     last = db.Column(db.String(50))
     county = db.Column(db.String(50))
     dob = db.Column(db.Integer)
     email = db.Column(db.String(50))
+    status = db.Column(db.String(20))
