79087ad869c4589f6bb01ad16f07e92004b3a703
Victoria Chen <vtoriachen@gmail.com>
2020-09-20 07:54:24 -0400

    Edit index and views to show registration history

--- my_app/views.py
+++ my_app/views.py
@@ -6,11 +6,12 @@ import time
 
 @app.route("/")
 def index():
-    """db_users = User.query.all()
-    users_list = [{"First Initial":u.first, "Last Name":u.last, "Date of Birth":u.dob, "County":u.county, "Email":u.email} for u in db_users]"""
+    db_users = User.query.all()
+    users_list = [{"first":u.first, "last":u.last, "dob":u.dob, "county":u.county, "email":u.email} for u in db_users]
 
-
-    return render_template("index.html")
+    
+    #return render_template("index.html")
+    return render_template("index.html", views_out = reversed(users_list))
     
 
 @app.route("/add_user", methods=['POST'])
@@ -18,15 +19,15 @@ def add_user():
     if request.method == "POST":
         info = request.get_json()
         statusGood = check_status.check_status(first_initial=info['first'], last_name=info['last'], DOB=info['dob'], county=info['county'])
-        #new_user = User(first=info['first'], last=info['last'], dob=info['dob'], county=info['county'], email=info['email'])
-        #db.session.add(new_user)
-        #db.session.commit()
+        
         if statusGood:
-            return "yes"
-        else:
-            return "no"
-    else:
-        return render_template("index.html")
+            new_user = User(first=info['first'], last=info['last'], dob=info['dob'], county=info['county'], email=info['email'])
+            db.session.add(new_user)
+            db.session.commit()
+            #return redirect("/")
+        #else:
+        return redirect("/")
+
     
 
     
